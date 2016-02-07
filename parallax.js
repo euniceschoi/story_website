855e6e21063cb216b57069d77e8e94b0113b94c4
euniceschoi <euniceschoi93@gmail.com>
2016-02-06 23:04:14 -0800

    add diamond for middle ground object and add parallax scrolling

--- parallax.js
+++ parallax.js
@@ -8,6 +8,6 @@ $(document).ready(function() {
   function parallaxScroll(){
   var scrolled = $(window).scrollTop();
   $('#bg-1').css('top',(0-(scrolled*.25))+'px');
-  // $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+  $('#bg-2').css('top',(0-(scrolled*.5))+'px');
   // $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
 }
