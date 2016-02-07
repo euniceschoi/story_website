058476aca52ed2abaf785922a4cb8ac353109c18
euniceschoi <euniceschoi93@gmail.com>
2016-02-06 22:11:11 -0800

    add javascript to allow parallax scrolling of star specks as I scroll down

---
+++ parallax.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(window).bind('scroll',function(e){
+      parallaxScroll();
+    // redrawDotNav();
+    });
+
+})
+  function parallaxScroll(){
+  var scrolled = $(window).scrollTop();
+  $('#bg-1').css('top',(0-(scrolled*.25))+'px');
+  // $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+  // $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+}
